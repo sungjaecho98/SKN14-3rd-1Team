e470db3f564c510e40fa6d6d99f466c3faa04efe
SANGHEEMOON <mshhhhh0000@naver.com>
2025-07-15 09:10:49 +0900

    update: 에러 수정

--- MoonSangHee/api_test.py
+++ MoonSangHee/api_test.py
@@ -1,9 +1,10 @@
-import requests, json, math
+import requests, json, math, os
 from pprint import pprint
+from dotenv import load_dotenv
 
-end_point = 'https://apis.data.go.kr/1471000/HtfsInfoService03'
-ENCODING = 'COVqDxtrNsa1PEgImSCkU%2B9ncLCoNr4m%2Bh6kiSZX%2B8dTB5pAB9VgCV1y0B8OqZvYpiMjOBMHGId7NJlJPAALwA%3D%3D'
-decoding = 'COVqDxtrNsa1PEgImSCkU+9ncLCoNr4m+h6kiSZX+8dTB5pAB9VgCV1y0B8OqZvYpiMjOBMHGId7NJlJPAALwA=='
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 url = 'http://apis.data.go.kr/1471000/HtfsInfoService03/getHtfsItem01?'
 
@@ -12,7 +13,7 @@ num_of_rows = 10
 params = {
     "pageNo":    "1",          # 페이지 번호
     "numOfRows": str(num_of_rows),         # 한 페이지 결과 수
-    "ServiceKey": decoding, # Swagger에 표시된 정확한 이름(ServiceKey)
+    "ServiceKey": API_KEY, # Swagger에 표시된 정확한 이름(ServiceKey)
     "type":      "json",       # 응답 포맷(xml/json) – default: xml
     # (필요시 API별 추가 파라미터를 여기에 더합니다)
 }
@@ -23,21 +24,29 @@ response.raise_for_status()
 body = response.json()["body"]
 total_count = int(body.get("totalCount", 0))
 total_pages = math.ceil(total_count / num_of_rows)
-# for page in range(0, total_count, num_of_rows):
-# for page in range(0, total_pages+1):
-for page in range(0, 3):
+
+items_section = body.get("items", [])
+
+# for page in range(1, total_pages):
+for page in range(1, 4):
+
     params["pageNo"] = str(page)
+    # print(params['pageNo'])
     resp = requests.get(url, params=params, timeout=10)
     resp.raise_for_status()
-    items = resp.json()["body"]["items"]["item"]
-    # items 처리(저장 등)
-    print(f"{page}페이지: {len(items)}개 처리")
+    body_ = resp.json()["body"]
+    items_section = body_.get("items", [])
+    
+    if isinstance(items_section, list):
+        items_list = [ entry["item"] for entry in items_section ]
+    
+    else:
+        items_list = [ items_section["item"] ]
 
-with open("output.jsonl", "w", encoding="utf-8") as f:
-    for page in range(1, total_pages+1):
-        params["pageNo"] = str(page)
-        resp = requests.get(url, params=params, timeout=10)
-        items = resp.json().get("response", {}).get("body", {}).get("items", {}).get("item", [])
-        for item in items:
-            f.write(json.dumps(item, ensure_ascii=False) + "\n")
-# pprint(response.json())
+    # pprint(items_list)
+    
+    with open('output.jsonl', 'a+', encoding='utf-8') as f:
+        for item in items_list:
+            f.write(json.dumps(item, ensure_ascii=False) + '\n')
+
+    print(f"{page}페이지: {len(items_list)}개 처리")

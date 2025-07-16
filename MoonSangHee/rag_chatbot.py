2852f8298148cc6837d0bb6f65eb88fd9c064962
SANGHEEMOON <mshhhhh0000@naver.com>
2025-07-16 09:52:34 +0900

    Feat: Fianl안에 파일들 수정

--- MoonSangHee/rag_chatbot.py
+++ MoonSangHee/rag_chatbot.py
@@ -1,8 +1,5 @@
 from config import load_config
-# from langchain_community.embeddings import OpenAIEmbeddings
-# from langchain_community.vectorstores import Pinecone
 import os, json
-from langchain.schema import Document
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from pinecone import Pinecone, ServerlessSpec
@@ -31,27 +28,33 @@ class RAG_Chatbot():
         self.pc = Pinecone(api_key=self.pinecone_api_key, environment=self.pinecone_env)
         self.vector_store = PineconeVectorStore(index=self.pc.Index(self.index_name), embedding=self.embeddings)
         self.retriever = self.vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 3}) 
-        self.prompt = self.prompt()
+        # self.prompt = self.prompt()
 
 
-    def run(self, question, retriever, model_name, temperature=0.3, max_token=512): 
+    def run(self, question, temperature=0.3, max_token=1024): 
 
         
-        llm = ChatOpenAI(openai_api_key=self.openai_api_key, temperature=temperature, model_name=model_name, max_tokens=max_token)
+        llm = ChatOpenAI(openai_api_key=self.openai_api_key, temperature=temperature, model_name=self.model_name, max_tokens=max_token)
+        retrieved_docs = retriever.get_relevant_documents(question)
+        context = "\n---\n".join([doc.page_content for doc in retrieved_docs])
+        prompt_template = self.prompt(question=question, context=context)
 
-        qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, chain_type="stuff", return_source_documents=True)
-        output = qa_chain.invoke({"query": question})
+        # qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, chain_type="stuff", return_source_documents=True)
+        # output = qa_chain.invoke({"query": question})
 
-        return output
-                                 
+        response = llm.invoke(prompt_template.format(question=question, context=context))
 
-    def prompt(self):
-        system_prompt = PromptTemplate.from_template("""
-        [System Instruction]
+        # return {'answer': response.content, 'source_documents': retrieved_docs}
+        return response.content                        
+
+    def prompt(self, question, context):
+        system_prompt = PromptTemplate.from_template(f"""
+        
+         [System Instruction]
         - 당신은 여러문서를 분석하여 사용자의 질문에 친절히 답변하는 건강기능식품 전문가이다.
         - 만약, 증상을 입력하면 증상에 대한 공감을 먼저 한 뒤, 제품을 추천해줘야 한다.
         - 추천한 제품은 자세하게 설명해줘야 한다.
-        - 제품을 추천할 시, 3개의 제품을 추천해야 한다. 만약, 3개의 제품이 없다면, 있는 만크만 추천해준다
+        - 제품을 추천할 시, 3개의 제품을 추천해야 한다. 만약, 3개의 제품이 없다면, 있는 만큼만 추천해준다
         - 답변은 반드시 [Example - Output Indicator]에 따라야 한다.
         - 주어진 문서내에서만 정보를 추출해 답변해야 한다.
         - 사용자의 질문에 대한 내용을 주어진 문서상에서 찾을 수 없는 경우 찾을수 없다고 답변해야 한다.
@@ -77,10 +80,10 @@ class RAG_Chatbot():
         """)
         return system_prompt
 
-cfg = load_config()    
-rag = RAG_Chatbot(cfg)
-question = '아, 똥이 안나와'
-retriever = rag.retriever
-res = rag.run(question, retriever, cfg['OPENAI_MODEL_NAME'])
-print(res)
+# cfg = load_config()    
+# rag = RAG_Chatbot(cfg)
+# question = '비타민 중에 임산부도 먹을 수 있는 제품 추천해줘'
+# retriever = rag.retriever
+# res = rag.run(question, retriever, cfg['OPENAI_MODEL_NAME'])
+# print(res['answer'])
 # pprint(retriever.invoke('피로개선에 도움이 되는 영양제는?'))
